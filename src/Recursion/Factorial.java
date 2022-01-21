f8edf556faf7d7dde07374fae222a3a119b72bdf
Abhishek Bansal <abhishekbansal1312@gmail.com>
2022-01-21 16:05:36 +0530

    Factorial of a number

---
+++ src/Recursion/Factorial.java
@@ -0,0 +1,32 @@
+package Recursion;
+
+import java.util.Scanner;
+
+public class Factorial {
+
+    // Using Head Recursion
+
+//    public static int fac(int n){
+//        if(n==0 || n==1){
+//            return 1;
+//        }
+//        return n*fac(n-1);
+//    }
+
+    // Using Tail Recursion
+
+    public static int fac(int n, int sum){
+        if(n==0 || n==1){
+            return sum;
+        }
+        return fac(n-1,sum*n);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the value");
+        int n = sc.nextInt();
+//      System.out.println(fac(n));  // For head recursion
+        System.out.println(fac(n,1));
+    }
+}
