40410ac2bff9ef06589d05bd8362024907028836
Abhishek Bansal <abhishekbansal1312@gmail.com>
2022-01-26 13:46:41 +0530

    check if string formed using given set of rules

--- src/Recursion/Q10.java
+++ src/Recursion/Q10.java
@@ -37,7 +37,7 @@ public class Q10 {
     }
     public static void main(String[] args) {
         Scanner s=new Scanner(System.in);
-        String str=s.nextLine();
+        String str=s.next();
         System.out.println(Seq(str));
     }
 }
