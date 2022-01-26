4890432557af45b9f75ef6e480212400eab2073f
Abhishek Bansal <abhishekbansal1312@gmail.com>
2022-01-26 12:54:41 +0530

    SubSequences of string

---
+++ src/Recursion/Subsequence_in_string.java
@@ -0,0 +1,21 @@
+package Recursion;
+
+import java.util.Scanner;
+
+public class Subsequence_in_string {
+    public static void subseq(String s, String res){
+        if(s.length()==0){
+            System.out.println(res);
+            return;
+        }
+        subseq(s.substring(1),res+s.charAt(0));
+        subseq(s.substring(1),res + "");
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("ENTER THE STRING");
+        String s = sc.next();
+        subseq(s,"");
+    }
+}
