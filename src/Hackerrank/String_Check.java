2834586ca13b8815f29de22aefe8a794351f7640
Abhishek Bansal <abhishekbansal1312@gmail.com>
2022-01-28 10:04:02 +0530

    check if all the letters of the English Alphabet are present oe not

---
+++ src/Hackerrank/String_Check.java
@@ -0,0 +1,22 @@
+package Hackerrank;
+
+/*Emily has been given an assignment to check if all the letters of the English Alphabet are present in the given sentence or not.
+ Help her recognize it by writing a code. You can ignore cases i.e no case-sensitivity required.
+ */
+
+import java.util.Scanner;
+
+public class String_Check {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s1 = sc.nextLine();
+        String s = s1.toLowerCase();
+
+        int c=0;
+        for(char i = 'a'; i <= 'z'; i++){
+            if(s.contains(Character.toString(i))) c++;
+        }
+        if(c==26) System.out.println("Yes");
+        else System.out.println("No");
+    }
+}
