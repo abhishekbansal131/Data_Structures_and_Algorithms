646c445c738ac527a0b9d80f10cd8ad3594d14f7
Abhishek Bansal <abhishekbansal1312@gmail.com>
2022-02-02 15:23:51 +0530

    Insertion and deletion operations on singly linked list

--- src/LinkedList/LL_Oper.java
+++ src/LinkedList/LL_Oper.java
@@ -1,5 +1,7 @@
 package LinkedList;
 
+import java.util.Scanner;
+
 class Node1{
     int data;
     Node1 next;
@@ -10,10 +12,21 @@ class Node1{
 
 public class LL_Oper {
     Node1 head;
+    public void creatLinkedList(){
+        Node1 node1 = new Node1(10);
+        Node1 node2 = new Node1(20);
+        Node1 node3 = new Node1(30);
+        Node1 node4 = new Node1(40);
+        Node1 node5 = new Node1(60);
+
+        head = node1;
+        node1.next = node2;
+        node2.next = node3;
+        node3.next = node4;
+        node4.next = node5;
+    }
     public void insert_at_last(Node1 n){
-        if(head==null){
-            head = n;
-        }
+        if(head==null) head =n;
         else {
             Node1 temp =head;
             while (temp.next!=null) {
@@ -37,6 +50,7 @@ public class LL_Oper {
         temp.next = n;
     }
 
+
     public void delete_from_begin(){
         if(head == null) System.out.println("Empty linked list");
         else {
@@ -48,7 +62,7 @@ public class LL_Oper {
         if(head == null) System.out.println("Empty linked list");
         else{
             Node1 temp = head;
-            while (temp!=null){
+            while (temp.next!=null){
                 temp = temp.next;
                 if(temp.next.next == null) break;
             }
@@ -77,20 +91,45 @@ public class LL_Oper {
     }
 
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         LL_Oper obj = new LL_Oper();
-        Node1 node1 = new Node1(10);
-        Node1 node2 = new Node1(20);
-        Node1 node3 = new Node1(30);
-        Node1 node4 = new Node1(40);
-        obj.head = node1;
-        node1.next = node2;
-        node2.next = node3;
-        node3.next = node4;
-        //obj.insert_at_last(new Node1(7));
-        //obj.insert_at_begin(new Node1(1));
-        //obj.delete_from_begin();
-        //obj.insert_at_any(new Node1(100),3);
-        //obj.delete_from_any(3);
-        obj.display();
+        obj.creatLinkedList();
+        while(true){
+            System.out.println("""
+                    PRESS 1 FOR INSERTION AT BEGIN, 2 FOR INSERTION AT END AND 3 FOR INSERTION AT ANY SPECIFIC POSITION.
+                    PRESS 4 FOR DELETION FROM BEGIN, 5 FOR DELETION AT END AND 6 FOR DELETION FOR ANY SPECIFIC POSITION.
+                    press 7 for exit""");
+            int choice = sc.nextInt();
+            if(choice==1){
+                System.out.println("What value do you want to insert?\nEnter the value");
+                int data = sc.nextInt();
+                obj.insert_at_begin(new Node1(data));
+            }
+            else if(choice==2){
+                System.out.println("What value do you want to insert?\nEnter the value");
+                int data = sc.nextInt();
+                obj.insert_at_last(new Node1(data));
+            }
+            else if(choice==3){
+                System.out.println("What value do you want to insert?\nEnter the value");
+                int data = sc.nextInt();
+                System.out.println("Enter the specific location");
+                int p = sc.nextInt();
+                obj.insert_at_any(new Node1(data),p);
+            }
+            else if(choice==4) obj.delete_from_begin();
+            else if(choice==5) obj.delete_from_end();
+            else if(choice==6){
+                System.out.println("Enter the position of node which you want to delete");
+                int p = sc.nextInt();
+                obj.delete_from_any(p);
+            }
+            else if(choice==7){
+                System.out.println("Thank you");
+                break;
+            }
+            else System.out.println("please enter valid choice");
+            obj.display();
+        }
     }
 }
