1baa98bc4c04427344271b9b25d800d8c32f1438
Abhishek Bansal <abhishekbansal1312@gmail.com>
2022-02-03 22:25:17 +0530

    String check

--- src/Hackerrank/String_Check.java
+++ src/Hackerrank/String_Check.java
@@ -1,8 +1,8 @@
 package Hackerrank;
 
 /*Emily has been given an assignment to check if all the letters of the English Alphabet are present in the given sentence or not.
- Help her recognize it by writing a code. You can ignore cases i.e no case-sensitivity required.
- */
+Help her recognize it by writing a code. You can ignore cases i.e no case-sensitivity required.
+*/
 
 
 import java.util.Scanner;
@@ -12,7 +12,6 @@ public class String_Check {
         Scanner sc = new Scanner(System.in);
         String s1 = sc.nextLine();
         String s = s1.toLowerCase();
-
         int c=0;
         for(char i = 'a'; i <= 'z'; i++){
             if(s.contains(Character.toString(i))) c++;
