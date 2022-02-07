9b04931107a1034b4b786e71f23cc8dfbc3e7434
Abhishek Bansal <abhishekbansal1312@gmail.com>
2022-02-07 17:56:12 +0530

    Mid node of linked list

--- src/LinkedList/MidPoint.java
+++ src/LinkedList/MidPoint.java
@@ -12,6 +12,7 @@ class Node8<T>{
 }
 public class MidPoint {
     Node8<Integer> head;
+    int c=0;
     public void creatLinkedlist(){
         Scanner sc = new Scanner(System.in);
         ArrayList<Node8<Integer>> a = new ArrayList<>();
@@ -20,26 +21,15 @@ public class MidPoint {
             int data = sc.nextInt();
             if(data == -1) break;
             a.add(new Node8<>(data));
+            c++;
             if(i>0) a.get(i-1).next = a.get(i);
             i++;
         }
         if(a.size()!=0) head = a.get(0);
     }
-    public int count(){
-        int c=0;
-        if(head==null) return 0;
-        else {
-            Node8<Integer> temp = head;
-            while(temp!=null){
-                c++;
-                temp= temp.next;
-            }
-        }
-        return c;
-    }
     public void midNode(){
-        int m = count()/2;
-        if(count()%2==0) m--;
+        int m = c/2;
+        if(c%2==0) m--;
         Node8<Integer> temp = head;
         for(int i=0; i<m; i++) temp = temp.next;
         System.out.println(temp.data);
