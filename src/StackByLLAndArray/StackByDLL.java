405939a5aef408b8b29d215f807176f6f8bcab10
Abhishek Bansal <abhishekbansal1312@gmail.com>
2022-02-08 00:59:47 +0530

    Stack using doubly linked list

---
+++ src/StackByLLAndArray/StackByDLL.java
@@ -0,0 +1,53 @@
+package StackByLLAndArray;
+import Nodes.Node1;
+
+public class StackByDLL {
+    Node1 top;
+    public boolean isEmpty() {
+        return top == null;
+    }
+    public void push(int data){
+        Node1 n = new Node1();
+        n.setData(data);
+        if(isEmpty()){
+            top = n;
+        }
+        else {
+            n.setNext(top);
+            top.setPrev(n);
+            top = n;
+        }
+    }
+    public void pop(){
+        if(isEmpty()) System.out.println("Empty Stack");
+        else {
+            if(top.getNext() == null) top = null;
+            else {
+                top.getNext().setPrev(null);
+                top = top.getNext();
+            }
+        }
+    }
+    public void display(){
+        if(top == null) System.out.println("Empty stack");
+        else {
+            Node1 temp = top;
+            while(temp != null){
+                System.out.println(temp.getData());
+                temp = temp.getNext();
+            }
+        }
+    }
+    public static void main(String[] args) {
+        StackByDLL obj = new StackByDLL();
+        obj.push(20);
+        obj.push(40);
+//        obj.push(60);
+//        obj.push(80);
+//        obj.pop();
+//        obj.pop();
+//        obj.pop();
+//        obj.pop();
+        obj.display();
+    }
+}
