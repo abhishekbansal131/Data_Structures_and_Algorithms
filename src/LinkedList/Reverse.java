85ae489b3565ccc404a292e3fb92314969302259
Abhishek Bansal <abhishekbansal1312@gmail.com>
2022-02-27 14:26:04 +0530

    REVERSE A LINKED LIST

--- src/LinkedList/Reverse.java
+++ src/LinkedList/Reverse.java
@@ -1,8 +1,5 @@
 package LinkedList;
 
-import java.util.ArrayList;
-import java.util.Scanner;
-
 class Node10<T>{
     T data;
     Node10<T> next;
@@ -12,19 +9,17 @@ class Node10<T>{
 }
 public class Reverse {
     Node10<Integer> head;
-    public void creatLinkedlist(){
-        Scanner sc = new Scanner(System.in);
-        ArrayList<Node10<Integer>> a = new ArrayList<>();
-        int i=0;
-        while(true){
-            int data = sc.nextInt();
-            if(data == -1) break;
-            a.add(new Node10<>(data));
-            if(i>0) a.get(i-1).next = a.get(i);
-            i++;
+    public void insertAtLAst(Node10<Integer> n){
+        if(head == null) head = n;
+        else{
+            Node10<Integer> temp = head;
+            while(temp.next != null){
+                temp = temp.next;
+            }
+            temp.next = n;
         }
-        if(a.size()!=0) head = a.get(0);
     }
+    // Iterative Approach
     public void rev(){
         Node10<Integer> temp = head, prev = null, n;
         while(temp != null){
@@ -34,21 +29,37 @@ public class Reverse {
             temp = n;
         }
         head = prev;
-        Node10<Integer> t = head;
-        while (t!=null){
-            System.out.println(t.data);
-            t = t.next;
+    }
+    //RECURSIVE APPROACH TO REVERSE A LINKED LIST
+    public Node10<Integer> rev2(Node10<Integer> head){
+        if(head == null) return head;
+        if (head.next == null) return head;
+        Node10<Integer> n = rev2(head.next);
+        head.next.next = head;
+        head.next = null;
+        return n;
+    }
+    public void display(){
+        //rev2(head);
+        if(head == null) System.out.println("EMPTY");
+        else {
+            Node10<Integer> temp = head;
+            while(temp != null){
+                System.out.println(temp.data);
+                temp = temp.next;
+            }
         }
     }
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int t = sc.nextInt();
-        while(t!=0) {
-            Reverse obj = new Reverse();
-            obj.creatLinkedlist();
-            obj.rev();
-            t--;
-        }
+        Reverse obj = new Reverse();
+        obj.insertAtLAst(new Node10<>(1));
+        obj.insertAtLAst(new Node10<>(2));
+        obj.insertAtLAst(new Node10<>(3));
+        obj.insertAtLAst(new Node10<>(4));
+        obj.insertAtLAst(new Node10<>(5));
+        //obj.rev();
+        obj.head = obj.rev2(obj.head);
+        obj.display();
     }
 }
